7a3b32f16f1cf883c9f6555b77c3d57b2c9a864d
Your Name <2000bhaveshkumar@gmail.com>
2022-03-23 01:10:47 +0530

    Fresh commit

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -24,7 +24,7 @@ function Login() {
           alt=""
         />
         <div className="login__text">
-          <h1>Sign in to WhatsApp</h1>
+          <h1>Sign in to ChatApp</h1>
         </div>
         <Input
           type="text"
